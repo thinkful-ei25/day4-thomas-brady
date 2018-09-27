283893278650ff39f84138b51415d4a55cb672fe
Box <bradycfox@gmail.com>
2018-09-27 16:31:44 -0400

    cleaned up etch.js file a bit.

--- the-etcher-challenge/etch.js
+++ the-etcher-challenge/etch.js
@@ -1,18 +1,10 @@
 
 'use strict';
 
-//When user hovers over a grid cell - the "active" class should be permanetly applied to the cell.
-//When the user clicks the "clear grid" button, the grid should reset all cells by calling the provided createAndPlaceRows() function.
-
-/* Approach #1: Add an event listener onto every .cell that's listening for the appropriate event. Notice that since createAndPlaceRows() unbinds all listeners on .cell, you need to reapply them every time it's invoked. */
-
-
 
 // Add Event Listeners here:
 
 function handleHover() {
-    //listen for hover on all class cell
-    //switch up class to the current item
     $('.grid').on('mouseover', '.cell', function() {
         $(this).addClass('active');
     });
@@ -32,8 +24,6 @@ $(() => {
     createAndPlaceRows(8);
     handleHover();
     handleReDraw(8);
-  
-    // Bind your event listeners here:
 });
   
   
